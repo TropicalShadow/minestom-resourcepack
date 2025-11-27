d473594de4e4e1448a644bd71669af626cc24507
TropicalShadow <me@tesseract.club>
2025-11-27 19:26:09 +0000

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,62 @@
+plugins {
+    id("java")
+    alias(libs.plugins.shadow)
+    application
+}
+
+group = "club.tesseract.minestom"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+
+    // luckperms
+    maven("https://repo.hypera.dev/snapshots/")
+    maven("https://repo.tesseract.club/releases/")
+    maven("https://repo.lucko.me/") // spark-common
+    maven("https://oss.sonatype.org/content/repositories/snapshots/") // spark-common's dependencies
+}
+
+dependencies {
+    implementation(libs.minestom)
+    implementation(libs.minestom.utils)
+
+    implementation(libs.logback.classic)
+    implementation(libs.jansi)
+
+    implementation(libs.lombok)
+    annotationProcessor(libs.lombok)
+
+    /* Spark */
+    implementation(libs.minestom.spark){
+        exclude(group = "me.lucko", module = "bytesocks-java-client")
+    }
+    implementation(libs.bytesocks.java.client)
+    /* End of Spark */
+
+    /* Luckperms */
+    implementation(libs.luckperms)
+    implementation(libs.sponge.configurate.core)
+    implementation(libs.sponge.configurate.hocon)
+    implementation(libs.hikari)
+    implementation(libs.h2database) // Replace with database provider of your choice
+    /* End of Luckperms */
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(25))
+    sourceCompatibility = JavaVersion.VERSION_25
+    targetCompatibility = JavaVersion.VERSION_25
+}
+
+tasks{
+
+    shadowJar{
+        archiveClassifier.set("")
+        mergeServiceFiles()
+    }
+
+    compileJava{
+        options.encoding = "UTF-8"
+    }
+}
